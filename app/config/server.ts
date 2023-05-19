71d50681a023b2ef102983489843b8e1b15a7bf5
sufng <129497982+sufng@users.noreply.github.com>
2023-05-19 09:51:06 +0800

    Update server.ts

--- app/config/server.ts
+++ app/config/server.ts
@@ -33,8 +33,11 @@ export const getServerSideConfig = () => {
     );
   }
 
-  return {
-    apiKey: process.env.OPENAI_API_KEY,
+const apiKeys = (process.env.OPENAI_API_KEY ?? '').split(',')
+const apiKey = apiKeys.at(Math.floor(Math.random() * apiKeys.length)) ?? ''
+  
+return {
+  apiKey,
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
