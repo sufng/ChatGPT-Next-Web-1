89eae07ffd3b0627ba6b51708fa0554bd6a63270
sufng <129497982+sufng@users.noreply.github.com>
2023-05-19 10:37:25 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -172,7 +172,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "感谢支持本项目!💗
+    BotHello: "感谢支持本项目!💗     
 📕**[点击此处查看使用教程](https://pigat4iq0eo.feishu.cn/wiki/NedGwFQaAiL0OTk1OJccnyUZnbd)**",
     Error: "出错了，稍后重试吧",
     Prompt: {
