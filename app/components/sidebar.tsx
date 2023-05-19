844869c2cf04104c4449860d03d18e0701398628
sufng <129497982+sufng@users.noreply.github.com>
2023-05-19 17:39:11 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href="https://pigat4iq0eo.feishu.cn/wiki/YOVOwALfwilAubkin5ec8RMCnKh" target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
